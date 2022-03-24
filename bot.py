6ca44c2ed5b4f3e24424e6d7ab8afc7c7c7cd861
Serewaya <58146873+Serewaya@users.noreply.github.com>
2022-03-24 18:43:41 -0400

    update

--- bot.py
+++ bot.py
@@ -1,8 +1,14 @@
-import slack
 import os
-from flask import Flask
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-client = slack.WebClient(token=os.environ["SLACK_TOKEN"])
+# Initializes your app with your bot token and socket mode handler
+app = App(token=os.environ.get("SLACK_TOKEN"))
 
-client.chat_postMessage(channel='general', text="Currently sending message after being deployed on heroku :)")
+# Start your app
+if __name__ == "__main__":
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+
+
+app.chat_postMessage(channel='general', text="Currently sending message after being deployed on heroku :)")
     
