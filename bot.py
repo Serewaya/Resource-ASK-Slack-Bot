0f42ab874e5960dcd5251bebbc9a210f717194e2
Serewaya <58146873+Serewaya@users.noreply.github.com>
2022-03-24 17:15:12 -0400

    update

--- bot.py
+++ bot.py
@@ -2,12 +2,24 @@ import slack
 import os
 from pathlib import Path
 from flask import Flask
+from dotenv import load_dotenv
 from slackeventsapi import SlackEventAdapter
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
 slack_event_adapter = SlackEventAdapter(os.environ["SIGNING_SECRET"],'/slack/events', app)
 
 client = slack.WebClient(token=os.environ["SLACK_TOKEN"])
 
+@ slack_event_adapter.on('message')
+def message(payload):
+    event = payload.get('event', {})
+    channel_id = event.get('channel')
+    user_id = event.get('user')
+    text = event.get('text')
+    client.chat_postMessage(channel=channel_id, text=text)
+    
 if __name__ == "__main__":
     app.run(debug=True)
