aee2b78dddedf9d9389de20ffa4128a7afbd2f43
Serewaya <58146873+Serewaya@users.noreply.github.com>
2022-03-25 07:34:52 -0400

    update

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from keep_alive import keep_alive
+
 
 # Initializes your app with your bot token and socket mode handler
 app = App(token=os.environ.get("SLACK_SECRET"))
@@ -14,8 +16,7 @@ def message_hello(message, say):
     say(f"Hey there <@{message['user']}>!")
  
 # Start your app
-if __name__ == "__main__":
-    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+keep_alive()
 
 
     
