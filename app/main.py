aff244f217a5d03630f49307a763bad41445ed6e
Serewaya <58146873+Serewaya@users.noreply.github.com>
2022-03-25 08:51:46 -0400

    update

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 
 from flask import Flask
  
-app = Flask(__name__)
+client = Flask(__name__)
  
-@app.route("/")
+@client.route("/")
 def home_view():
         return "<h1>Currently Running :)</h1>"
--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-
+from app.main import client
 # Initializes your app with your bot token and socket mode handler
-app = App(token=os.environ.get("SLACK_TOKEN"))
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
 # Listens to incoming messages that contain "hello"
 # To learn available listener arguments,
@@ -15,6 +15,5 @@ def message_hello(message, say):
 
 # Start your app
 if __name__ == "__main__":
-    SocketModeHandler(app, os.environ["SLACK_TOKEN"]).start()
-
-    
+    client.run()
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()    
