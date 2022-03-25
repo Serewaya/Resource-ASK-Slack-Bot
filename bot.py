8910a44e093aeb35dcea705d743cce42bc1de535
Serewaya <58146873+Serewaya@users.noreply.github.com>
2022-03-24 21:29:37 -0400

    update

--- bot.py
+++ bot.py
@@ -1,19 +1,21 @@
 import os
 from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-# Initializes your app with your bot token and signing secret
-app = App(
-    token=os.environ.get("SLACK_TOKEN"),
-    signing_secret=os.environ.get("SIGNING_SECRET")
-)
+# Initializes your app with your bot token and socket mode handler
+app = App(token=os.environ.get("SLACK_SECRET"))
 
+# Listens to incoming messages that contain "hello"
+# To learn available listener arguments,
+# visit https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
 @app.message("hello")
 def message_hello(message, say):
     # say() sends a message to the channel where the event was triggered
     say(f"Hey there <@{message['user']}>!")
 
+# Start your app
 if __name__ == "__main__":
-    app.start(debug=True)
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
 
 
     
