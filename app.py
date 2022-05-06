fd831289ac68dd3cecca51d30848c0b2c734b547
Serewaya <58146873+Serewaya@users.noreply.github.com>
2022-05-06 16:59:25 -0400

    update

--- app.py
+++ app.py
@@ -13,7 +13,17 @@ import pymongo
 from pymongo import MongoClient 
 from linkpreview import link_preview
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from http.server import BaseHTTPRequestHandler
+from datetime import datetime
 
+class handler(BaseHTTPRequestHandler):
+
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+    return
 page =1
 
 cluster=MongoClient("mongodb+srv://projectask:wD4odl0AK8ahUmFc@cluster0.e0sdb.mongodb.net/discord?retryWrites=true&w=majority")
